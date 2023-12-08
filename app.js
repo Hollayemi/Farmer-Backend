a57cdc8bf6fe1dc5506a49f73308f0476e5fe57c
Stephen Olayemi <stephenyemmitty@gmail.com>
2023-12-08 12:05:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+require('dotenv').config();
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree');
+const orderRoutes = require('./routes/order');
+
+// app
+const app = express();
+
+// db
+let connection;
+let siteUrl;
+if (process.env.NODE_ENV === "development") {
+  connection = "mongodb://127.0.0.1:27017/corisio";
+  siteUrl = process.env.SITE_URL;
+} else {
+  connection =
+    "mongodb+srv://stephanyemmitty:_Holla83626@cluster0.pb3wcml.mongodb.net/corislo";
+  siteUrl = process.env.PROD_SITE_URL;
+}
+
+mongoose
+    .connect(connection, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB Connected'));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+// routes middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+app.use('/api', braintreeRoutes);
+app.use('/api', orderRoutes);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
